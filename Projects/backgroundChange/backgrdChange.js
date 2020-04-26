acacc173ba5f9cbeb637ed43587f304cc40befe1
max Zulfer <maxzulfer@MacBook-Pro-2.local>
2020-04-26 09:10:15 -0700

    added switch

--- Projects/backgroundChange/backgrdChange.js
+++ Projects/backgroundChange/backgrdChange.js
@@ -1,5 +1,33 @@
 
-document.addEventListener('click', function() {
-    document.getElementById('body').style.backgroundColor = 'red';
-    document.getElementById('button').style.backgroundColor = 'green';
-});
+const changeBackground = () => {
+    let newColor = '';
+    let x = Math.floor(Math.random() * 7);
+    switch (x) {
+        case 0:
+            newColor = 'red'
+            break;
+        case 1:
+            newColor = 'blue'
+            break;
+        case 2:
+            newColor = 'green'
+            break;
+        case 3:
+            newColor = 'yellow'
+            break;
+        case 4:
+            newColor = 'purple'
+            break;
+        case 5:
+            newColor = 'orange'
+            break;
+        case 6:
+            newColor = 'brown'
+            break;
+        case 7:
+            newColor = 'black'
+            break;
+    } 
+    let elem = document.getElementById('body');
+    elem.style.backgroundColor = newColor;
+}
