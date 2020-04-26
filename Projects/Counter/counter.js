ef180839fe8b6ce548eaf3e6844882e0a320b95d
max Zulfer <maxzulfer@MacBook-Pro-2.local>
2020-04-26 10:01:11 -0700

    operator buttons working

--- Projects/Counter/counter.js
+++ Projects/Counter/counter.js
@@ -3,21 +3,38 @@
 // turn red when negative/green when positive
 // count number 
 
-let mainCount = document.getElementById('count').innerHTML = 0;
-let lowerBtn = document.getElementById('btn 1')
-let addBtn = document.getElementById('btn 2')
+let mainCount = document.getElementById('count') 
+let count = 0;
 
-mainCount++
+let countColor = document.getElementById('count').innerHTML;
 
-function addCount() {
-    
+let lowerBtn = document.getElementById('btn 1');
+let addBtn = document.getElementById('btn 2');
+let zeroButton = document.getElementById('zeroBtn');
+
+
+
+lowerBtn.onclick = function() {
+    count--;
+    mainCount.innerHTML = count;
+};
+
+addBtn.onclick = function() {
+    count++;
+    mainCount.innerHTML = count;
 }
+zeroButton.onclick = function() {
+    count = 0;
+    mainCount.innerHTML = count;
+}
+
+console.log(count)
 
 
 const colorChange = () => {
-    if (mainCount < 0) {
+    if (count.value < 0) {
         document.getElementById('count').style = 'Color: red'
-    } else if (mainCount > 0) {
+    } else if (count.value > 0) {
         document.getElementById('count').style ='Color: green'
     } else {
         document.getElementById('count').style = 'Color: black'
@@ -27,4 +44,3 @@ colorChange()
 
 
 
-console.log(mainCount)
